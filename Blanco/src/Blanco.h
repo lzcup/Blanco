590736be94e47916e7b07aedb95152ee606db6ef
lzcup <1206936941@qq.com>
2024-05-22 21:27:47 +0800

    Add Log System

--- Blanco/src/Blanco.h
+++ Blanco/src/Blanco.h
@@ -2,6 +2,7 @@
 
 //Use for application
 #include "Blanco/Application.h"
+#include "Blanco/Log.h"
 
 //Entry Point
 #include "Blanco/EntryPoint.h"
---
+++ Blanco/src/Blanco/Log.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include <memory>
+
+namespace Blanco 
+{
+	class BL_API Log{
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; };
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; };
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+//log macro
+#define BL_CORE_TRACE(...)     Blanco::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define BL_CORE_INFO(...)      Blanco::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define BL_CORE_WARN(...)      Blanco::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define BL_CORE_ERROR(...)     Blanco::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define BL_CORE_CRITICAL(...)  Blanco::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define BL_TRACE(...)     Blanco::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define BL_INFO(...)      Blanco::Log::GetClientLogger()->info(__VA_ARGS__)
+#define BL_WARN(...)      Blanco::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define BL_ERROR(...)     Blanco::Log::GetClientLogger()->error(__VA_ARGS__)
+#define BL_CRITICAL(...)  Blanco::Log::GetClientLogger()->critical(__VA_ARGS__)
+
+
